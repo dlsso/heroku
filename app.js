dc127d7865c9f3f12647eb85d5d922d09cfe867e
dlsso <supertonesfan@hotmail.com>
2014-07-25 09:28:20 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('<h1>Hello Boulder</h1>');
+});
+
+// 1) Modify the port to use whatever heroku gives us
+var server = app.listen(process.env.port || 9323, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
